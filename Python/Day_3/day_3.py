31e3a55c29f8d2e9f94104c7f7665775999b8b8f
Christian Graabæk Steenfeldt <c.graabaek@gmail.com>
2021-12-06 07:19:06 +0100

    Added day 3

--- Python/Day_3/day_3.py
+++ Python/Day_3/day_3.py
@@ -1,6 +1,66 @@
+import numpy as np
+
 print("Advent Of Code - Day 3")
 
 PUZZLEINPUT = open('input.txt', 'r').read().split("\n")
 
-firsts = [w[0] for w in PUZZLEINPUT]
-print(firsts)
+gamma_rate = ''
+epsilon_rate = ''
+
+for i in range(len(PUZZLEINPUT[0])):
+    elem = [line[i] for line in PUZZLEINPUT]
+    unique, counts = np.unique(elem, return_counts=True)    
+    #result = dict(zip(unique, counts))
+    zeroes = counts[0]
+    ones = counts[1]
+    
+    if zeroes > ones:
+        gamma_rate += '0'
+        epsilon_rate += '1'
+    else:
+        gamma_rate += '1'
+        epsilon_rate += '0'
+
+## Part 2
+def get_rating(input, rating_type):
+    # type 0 = o2, type 1 = co2
+    rating_arr = input
+
+    for i in range(len(rating_arr[0])):
+        elem = [w[i] for w in rating_arr]
+
+        unique, counts = np.unique(elem, return_counts=True)
+
+        zeroes = int(counts[0])
+        if len(counts) > 1:
+            ones = int(counts[1])
+        else: 
+            ones = 0
+    
+        if len(rating_arr) > 1:
+            if zeroes > ones:
+                if rating_type == 0:
+                    rating_arr = list(filter(lambda x: int(x[i])==0, rating_arr))
+                else:
+                    rating_arr = list(filter(lambda x: int(x[i])==1, rating_arr))
+                    
+            elif ones > zeroes:
+                if rating_type == 0:
+                    rating_arr = list(filter(lambda x: int(x[i])==1, rating_arr))
+                else:
+                    rating_arr = list(filter(lambda x: int(x[i])==0, rating_arr))
+
+            elif ones == zeroes:
+                if rating_type == 0:
+                    rating_arr = list(filter(lambda x: int(x[i])==1, rating_arr))
+                else:
+                    rating_arr = list(filter(lambda x: int(x[i])==0, rating_arr))
+    
+    return rating_arr
+
+
+print(f'Part 1: Power consumption is {int(gamma_rate, 2)*int(epsilon_rate, 2)}')
+print(f'Part 2: Power consumption is {int(get_rating(PUZZLEINPUT,0)[0], 2)*int(get_rating(PUZZLEINPUT,1)[0], 2)}')
+
+
+
